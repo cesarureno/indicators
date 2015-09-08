7d920287d26f654e5ad4fc50ff24ccb3ff9606f6
Cesar Ureno <cesarurenomartinez@gmail.com>
2015-09-08 14:29:09 -0500

    Modify users & create login

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < Devise::RegistrationsController
+	def new
+		super
+	end
+	def create
+		super
+		#Logica de tu codigo
+		
+	end
+	def update
+		super
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908165817) do
+ActiveRecord::Schema.define(version: 20150908180432) do
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
@@ -26,17 +26,21 @@ ActiveRecord::Schema.define(version: 20150908165817) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "lastname"
-    t.string   "username"
-    t.string   "password"
-    t.integer  "organization_id"
-    t.integer  "user_type_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["organization_id"], name: "index_users_on_organization_id"
-  add_index "users", ["user_type_id"], name: "index_users_on_user_type_id"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
